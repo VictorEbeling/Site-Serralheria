a01e5faa4d796035d899aeda483de7f805d0e7b2
Victor <ebelingvictor@gmail.com>
2023-06-28 22:06:56 -0300

    final js

--- func.js
+++ func.js
@@ -2,18 +2,21 @@
 
 function iterateThroughClass(classToRemove, ClassToAdd){
     while (document.getElementsByClassName(classToRemove).length) {
-        document.getElementsByClassName(classToRemove).className = ClassToAdd;
+        document.getElementsByClassName(classToRemove)[0].classList.replace(classToRemove, ClassToAdd);
       }
 }
 
 
 
 document.getElementById('dark-mode-switch').addEventListener('click',()=>{
-    if (document.body.classList.contains('bg-custom-dark-yellow')) {
+    if (document.body.classList.contains('bg-dark')) {
+        document.body.classList.remove('bg-dark')
         document.getElementById('moon-svg').setAttribute('href', 'node_modules/bootstrap-icons/bootstrap-icons.svg#moon')
         iterateThroughClass('bg-custom-dark-yellow', 'bg-custom-yellow')
+        console.log('funfou')
     }
     else {
+        document.body.classList.add('bg-dark')
         document.getElementById('moon-svg').setAttribute('href', 'node_modules/bootstrap-icons/bootstrap-icons.svg#moon-fill')
         iterateThroughClass('bg-custom-yellow', 'bg-custom-dark-yellow')
 
