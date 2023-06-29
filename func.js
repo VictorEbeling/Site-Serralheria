02b2a53602eca7ef65a6e1b6afd14e448d7d065c
Victor <ebelingvictor@gmail.com>
2023-06-28 21:49:26 -0300

    louco nessa call

--- func.js
+++ func.js
@@ -1,23 +1,21 @@
 // theme switch example for demo
 
-function iterateThroughClass(listOfElements, classToRemove, ClassToAdd){
-    listOfElements.forEach(element => {
-        element.classList.replace(classToRemove, ClassToAdd)
-    });
+function iterateThroughClass(classToRemove, ClassToAdd){
+    while (document.getElementsByClassName(classToRemove).length) {
+        document.getElementsByClassName(classToRemove).className = ClassToAdd;
+      }
 }
 
 
 
 document.getElementById('dark-mode-switch').addEventListener('click',()=>{
-    if (document.body.classList.contains('dark')) {
-        document.body.classList.remove('dark')
+    if (document.body.classList.contains('bg-custom-dark-yellow')) {
         document.getElementById('moon-svg').setAttribute('href', 'node_modules/bootstrap-icons/bootstrap-icons.svg#moon')
-        iterateThroughClass(document.getElementsByClassName('bg-custom-dark-yellow'),'bg-custom-dark-yellow', 'bg-custom-yellow')
+        iterateThroughClass('bg-custom-dark-yellow', 'bg-custom-yellow')
     }
     else {
-        document.body.classList.add('dark')
         document.getElementById('moon-svg').setAttribute('href', 'node_modules/bootstrap-icons/bootstrap-icons.svg#moon-fill')
-        iterateThroughClass(document.getElementsByClassName('bg-custom-yellow'), 'bg-custom-yellow', 'bg-custom-dark-yellow')
+        iterateThroughClass('bg-custom-yellow', 'bg-custom-dark-yellow')
 
     }
 
